305f6b218bace12e526816048a3b0824bfe7a0a2
Jason Norris <jasonmnorris@gmail.com>
2013-02-19 16:05:16 -0600

    Can create, list and view posts

---
+++ NoSqlBlog.Web/Controllers/PostController.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Mvc;
+
+namespace NoSqlBlog.Web.Controllers
+{
+    using Core.Interfaces;
+    using System.Linq;
+    using Core.Models;
+
+    public class PostController : Controller
+    {
+        private readonly IPostRepository _postRepository;
+
+        public PostController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_postRepository.GetRecentPosts(10).ToArray());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Post post)
+        {
+            _postRepository.AddPost(post);
+            AddMessage("Post Added!");
+            return RedirectToAction("View", new {id = post.Id});
+        }
+
+        private void AddMessage(string message)
+        {
+            ViewBag.Message = message;
+        }
+
+        public ActionResult View(int id)
+        {
+            var post = _postRepository.GetById(id);
+            return View(post);
+        }
+    }
+}
