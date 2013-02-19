5bcd8ee2f7072bb2f48562ec4913f8e5f2e8678a
Jason Norris <jasonmnorris@gmail.com>
2013-02-19 12:52:26 -0600

    Create initial projects

---
+++ NoSqlBlog.UnitTests.Core/BlogTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace NoSqlBlog.UnitTests.Core
+{
+    [TestFixture]
+    public class BlogTests
+    {
+        [Test]
+        public void Blog_Cannot_Be_Created_Without_A_Name()
+        {
+            
+        }
+
+        [Test]
+        public void Blog_Cannot_Be_Created_Without_An_Owner()
+        {
+            
+        }
+
+        [Test]
+        public void Blog_Has_Created_At_Timestamp_When_Created()
+        {
+            
+        }
+    }
+}
---
+++ NoSqlBlog.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NoSqlBlog.Web.MvcApplication" Language="C#" %>
